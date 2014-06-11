018bd6468a12fe1e382dc1ff46bd6f7889ca44a2
Garrett Squire <garrettsquire@gmail.com>
2014-06-11 10:52:14 -0700

    added HTTP responses 200, 403, 404

--- tide_utils.c
+++ tide_utils.c
@@ -1,3 +1,4 @@
+#include <errno.h>
 #include <fcntl.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -41,12 +42,97 @@ char *parse_get(char *buf) {
     return file;
 }
 
+/* return a 404 response to the client
+*/
+void client_404(int sock) {
+    char *buf = safe_malloc(BUF);
+
+    strcpy(buf, "HTTP/1.1 404 Not Found\r\n");
+    strcat(buf, "<HTML><HEAD><TITLE>HTTP ERROR 404</TITLE></HEAD><BODY>");
+    strcat(buf,
+        "404 Not Found.  Your request could not be completed due to");
+    strcat(buf, " encountering HTTP error number 404.</BODY></HTML>");
+    strcat(buf, "\r\n");
+
+    if ((write(sock, buf, strlen(buf)) < 0)) {
+        free(buf);
+        server_error(sock, "writing 404 to client");
+    }
+
+    free(buf);
+}
+
+/* return a 403 response to the client
+*/
+void client_403(int sock)
+{
+    char *buf = safe_malloc(BUF);
+    strcpy(buf, "HTTP/1.1 403 Forbidden\r\n");
+    strcat(buf, "<HTML><HEAD><TITLE>HTTP ERROR 403</TITLE></HEAD><BODY>");
+    strcat(buf,
+    "403 Forbidden.  Your request could not be completed due to encountering");
+    strcat(buf, " HTTP error number 403.</BODY></HTML>");
+    strcat(buf, "\r\n");
+    
+    if ((write(sock, buf, strlen(buf)) < 0)) {
+        free(buf);
+        server_error(sock, "writing 403 to client");
+    }
+
+    free(buf);
+}
+
+/* a successfull HTTP 200 response
+*/
+void client_200(int sock, off_t file_len, const char *cont_type)
+{
+    // buffer used to write back to the socket
+    char *buf = safe_malloc(BUF);
+    char *flen = safe_malloc(BUF);
+    sprintf(flen, "%ld", file_len);
+
+    strcpy(buf, "HTTP/1.1 200 OK\r\n");
+    strcat(buf, "Content-Type: ");
+    strcat(buf, cont_type);
+    strcat(buf, "\r\nContent-Length: ");
+    strcat(buf, flen);
+    strcat(buf, "\r\nConnection: close");
+    strcat(buf, "\r\n\r\n");
+
+    if ((write(sock, buf, strlen(buf)) < 0)) {
+        free(buf);
+        free(flen);
+        server_error(sock, "writing 200 to client");
+    }
+
+    free(buf);
+    free(flen);
+}
+
+/* write the requested file back to the client
+*/
+void client_write_file(int sock, int fd) {
+    char *read_buf = safe_malloc(BUF);
+
+    ssize_t bytes;
+
+    while ((bytes = read(fd, read_buf, BUF)) > 0) {
+        if (write(sock, read_buf, bytes) < 0) {
+            free(read_buf);
+            close(fd);
+            server_error(sock, "writing file to client");
+        }
+    }
+
+    free(read_buf);
+    close(fd);
+}
+
 /* file reading function for writing back to client
+ * TODO add logging support for server errors here
 */
 void write_client(int sock, const char *path) {
     struct stat buf;
-
-    ssize_t bytes;
 
     char *rel_path = safe_malloc(BUF);
 
@@ -56,27 +142,23 @@ void write_client(int sock, const char *path) {
     int fd = open(rel_path, O_RDONLY);
     if (fd < 0) {
         free(rel_path);
-        server_error(sock, "opening requested file");
+        client_404(sock);
+        return;
     }
 
     if ((stat(rel_path, &buf)) < 0) {
         free(rel_path);
         close(fd);
-        server_error(sock, "stat of requested file");
+        if (errno == EACCES)
+            client_403(sock);
+        else
+            client_404(sock);
         return;
     }
 
-    char *read_buf = safe_malloc(BUF);
-    while ((bytes = read(fd, read_buf, BUF)) > 0) {
-        if (write(sock, read_buf, bytes) < 0) {
-            free(read_buf);
-            free(rel_path);
-            close(fd);
-            server_error(sock, "writing to client");
-        }
-    }
-
-    free(read_buf);
+    // successful request, print 200 and file
+    client_200(sock, buf.st_size, "text/plain");
+    client_write_file(sock, fd);
+    
     free(rel_path);
-    close(fd);
 }
